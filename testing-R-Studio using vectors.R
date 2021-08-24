3a04c25b9bb9262223c92ad22d01ec3253d42a13
Enrique Gallegos Pateiro <A01172884@itesm.mx>
2021-08-24 15:21:52 -0500

    new

---
+++ testing-R-Studio using vectors.R
@@ -0,0 +1,7 @@
+#codes for creating vectors using c() funtion()
+
+z<- c(1, 2)             #numeric vectors
+z<c(TRUE, FALSE)        #logical
+z<-C("x", "y", "z")     #character
+z<1:10                  #interger
+z<-Z(1+0i), (2+1i)      #complex
